db65d128dfa4b5a0d6b1e0d8a671c9eea2e26376
pablocaro <caro.pablo@gmail.com>
2016-04-27 08:41:37 -0700

    A few updates for the microphone, able to print frequency and amplitude to console

--- 160427-microphone/ViewController.swift
+++ 160427-microphone/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    let mic = AKMicrophone()
+    var tracker: AKFrequencyTracker?
+    var silence: AKBooster? // this is to silence the output
+    
+    let minimum: Double = 60
+    let maximum: Double = 800
+    
+    var timer = NSTimer()
+    var increment:Double = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        AKSettings.audioInputEnabled = true
+        tracker = AKFrequencyTracker(mic, minimumFrequency: minimum, maximumFrequency: maximum)
+        silence = AKBooster(tracker!, gain: 0)
+        AudioKit.output = silence
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: #selector(ViewController.measureFrequency), userInfo: nil, repeats: true)
+        
+
+        
+        AudioKit.start()
+        mic.start()
+        tracker?.start()
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func measureFrequency() {
+        print("NSTimer Fired \(increment)")
+        print("mic on: \(mic.isStarted)")
+        
+        if let frequency = tracker?.frequency {
+            print("frequency: \(frequency)")
+        }
+        
+        if let amplitude = tracker?.amplitude {
+            print("amplitude: \(amplitude)")
+        }
+        
+        increment += 0.5
+    }
 
 
 }
+
+
 
