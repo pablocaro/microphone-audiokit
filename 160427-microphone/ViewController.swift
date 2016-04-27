5cbe9cef9292034b84e75e9bc1d03679a49b1492
pablocaro <caro.pablo@gmail.com>
2016-04-27 01:44:13 -0700

    Initial Commit

---
+++ 160427-microphone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  160427-microphone
+//
+//  Created by Pablo Caro on 4/27/16.
+//  Copyright © 2016 Pablo Caro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
